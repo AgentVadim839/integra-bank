024baef849cf2df79d6351531c0b3163468a8316
AgentVadim839 <149328680+AgentVadim839@users.noreply.github.com>
2025-05-20 15:20:17 +0300

    Main dash redesign
    Still has to repair transactions showcase.

--- Integra Bank/backend/integra/src/main/java/com/bank/integra/controller/user/DashboardController.java
+++ Integra Bank/backend/integra/src/main/java/com/bank/integra/controller/user/DashboardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import java.util.Map;
 
+//TODO Мобство во вью. Любые транзакции будто на получение и пишет + и зелёным, а не - и красным. Фронтендеры мобы
 @RequestMapping("/user")
 @Controller
 public class DashboardController {
