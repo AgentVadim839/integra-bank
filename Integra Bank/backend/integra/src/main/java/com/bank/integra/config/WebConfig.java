284ecf1c9c76486b22b83f4d39505d15cda41882
AgentVadim839 <149328680+AgentVadim839@users.noreply.github.com>
2025-11-21 23:17:55 +0200

    Test
    test

--- Integra Bank/backend/integra/src/main/java/com/bank/integra/config/WebConfig.java
+++ Integra Bank/backend/integra/src/main/java/com/bank/integra/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// Временно офф
 public class WebConfig implements WebMvcConfigurer {
     private final UserStatusInterceptor userStatusInterceptor;
 
